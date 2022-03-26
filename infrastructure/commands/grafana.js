0d96daaf3588da878ffe4513db2f2d3dc4e5e50b
Sophie McGarity <sophiemcgarity7@gmail.com>
2022-03-25 20:33:03 -0700

    cli logs for stop and start grafana

--- infrastructure/commands/grafana.js
+++ infrastructure/commands/grafana.js
@@ -1,8 +1,17 @@
 import {
   // uploadTestScript,
-  runGrafanaTask
+  runGrafanaTask,
 } from "../utilities/runGrafanaCommand.js";
+
+/*
+  TO IMPLEMENT
+  error handling to check if a grafana task is already running
+  don't let the user start another
+*/
 
 (async () => {
   await runGrafanaTask();
+  setTimeout(() => {
+    console.log("Grafana Started");
+  }, 5000);
 })();
--- infrastructure/commands/stopGrafana.js
+++ infrastructure/commands/stopGrafana.js
@@ -2,4 +2,7 @@ import { stopGrafanaTask } from "../utilities/stopGrafanaCommand.js";
 
 (async () => {
   await stopGrafanaTask();
+  setTimeout(() => {
+    console.log("Grafana Stopped");
+  }, 10000);
 })();
