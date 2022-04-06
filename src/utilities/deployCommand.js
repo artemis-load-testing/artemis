47aa2ddb917ed8ae12e788ccf621cea2e1ae8acc
Charles A. Hartman <charles@hartman.dev>
2022-04-05 18:07:12 -0700

    increment version, comment out console debug in deploy, reset config.json to true for npm publish of 1.0.5

--- src/utilities/deployCommand.js
+++ src/utilities/deployCommand.js
@@ -1,19 +1,19 @@
-const AWS = require("aws-sdk");
-const execSync = require("child_process").execSync;
-const userRegion = execSync("aws configure get region").toString().trim();
+const AWS = require('aws-sdk');
+const execSync = require('child_process').execSync;
+const userRegion = execSync('aws configure get region').toString().trim();
 AWS.config.update({ region: userRegion });
 const cloudformation = new AWS.CloudFormation();
 const timestream = new AWS.TimestreamWrite();
-const stackName = "ArtemisAwsStack";
-const timestreamDbName = "artemis-db";
-const fs = require("fs");
-const path = require("path");
+const stackName = 'ArtemisAwsStack';
+const timestreamDbName = 'artemis-db';
+const fs = require('fs');
+const path = require('path');
 
-const ora = require("ora-classic");
-const chalk = require("chalk");
+const ora = require('ora-classic');
+const chalk = require('chalk');
 
-const { promisify } = require("util");
-const exec = promisify(require("child_process").exec);
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
 
 const artemisStackDeployed = async () => {
   const allStacks = await cloudformation.describeStacks().promise();
@@ -31,43 +31,43 @@ const getExistingArtemisDb = async () => {
   return databaseDeployed;
 };
 
-const cdkPath = path.join(__dirname, "../aws");
-const cdkPathEscaped = cdkPath.replace(/ /g, "\\ ");
+const cdkPath = path.join(__dirname, '../aws');
+const cdkPathEscaped = cdkPath.replace(/ /g, '\\ ');
 
 const startDeployment = async () => {
   const stackExists = await artemisStackDeployed();
   const artemisDatabase = await getExistingArtemisDb();
   if (stackExists && !!artemisDatabase) {
-    console.log("ArtemisAwsStack and artemis-db are already deployed.");
+    console.log('ArtemisAwsStack and artemis-db are already deployed.');
   } else if (!!artemisDatabase) {
-    console.log(chalk.green("Your artemis-db is already deployed."));
+    console.log(chalk.green('Your artemis-db is already deployed.'));
     const spinner = ora(
-      chalk.cyan("Deploying ArtemisAwsStack only...")
+      chalk.cyan('Deploying ArtemisAwsStack only...')
     ).start();
-    spinner.color = "yellow";
-    fs.readFileSync(`${cdkPath}/config.json`, "utf8");
+    spinner.color = 'yellow';
+    fs.readFileSync(`${cdkPath}/config.json`, 'utf8');
     await exec(
       `cd ${cdkPathEscaped} && cdk synth && cdk bootstrap && cdk deploy --require-approval never`,
       {
-        encoding: "utf-8",
+        encoding: 'utf-8',
       }
     );
     spinner.succeed(
-      chalk.cyan("Artemis infrastructure on AWS successfully deployed.")
+      chalk.cyan('Artemis infrastructure on AWS successfully deployed.')
     );
   } else {
     const spinner = ora(
-      chalk.cyan("Deploying ArtemisAwsStack and artemis-db...")
+      chalk.cyan('Deploying ArtemisAwsStack and artemis-db...')
     ).start();
-    spinner.color = "yellow";
+    spinner.color = 'yellow';
     await exec(
       `cd ${cdkPathEscaped} && cdk synth && cdk bootstrap && cdk deploy --require-approval never`,
       {
-        encoding: "utf-8",
+        encoding: 'utf-8',
       }
     );
     spinner.succeed(
-      chalk.cyan("Artemis infrastructure on AWS successfully deployed.")
+      chalk.cyan('Artemis infrastructure on AWS successfully deployed.')
     );
   }
 };
@@ -78,9 +78,9 @@ const setFirstDeployToFalse = async () => {
     if (err) console.log(err);
     else {
       let firstDeployStatus = JSON.parse(
-        fs.readFileSync(`${cdkPath}/config.json`, "utf8")
+        fs.readFileSync(`${cdkPath}/config.json`, 'utf8')
       ).firstDeploy;
-      console.log("File written successfully with value: ", firstDeployStatus);
+      // console.log("File written successfully with value: ", firstDeployStatus);
     }
   });
 };
