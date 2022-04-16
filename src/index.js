952a3d2e3fa5fc8e46874c66b92211796129a078
Mia <mjimenez@vt.edu>
2022-04-16 13:58:12 -0400

    Added a --yes flag to artemis teardown and artemis destroy-db so that a user doesn't have to confirm again in the console
    Co-authored-by: miajim <mjimenez@vt.edu>
    Co-authored-by: romero-c-raul <raulromerocano@aggienetwork.com>
    Co-authored-by: charleshartman <charles@hartman.dev>
    Co-authored-by: sophiemcgarity <sophiemcgarity7@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,55 +1,58 @@
 #!/usr/bin/env node
 
-const cli = require('commander');
-const start = require('./commands/start.js');
-const startGrafana = require('./commands/startGrafana.js');
-const stopGrafana = require('./commands/stopGrafana.js');
-const deploy = require('./commands/deploy.js');
-const teardown = require('./commands/teardown.js');
-const sleep = require('./commands/sleep.js');
-const destroyDb = require('./commands/destroydb.js');
+const cli = require("commander");
+const start = require("./commands/start.js");
+const startGrafana = require("./commands/startGrafana.js");
+const stopGrafana = require("./commands/stopGrafana.js");
+const deploy = require("./commands/deploy.js");
+const teardown = require("./commands/teardown.js");
+const sleep = require("./commands/sleep.js");
+const destroyDb = require("./commands/destroydb.js");
 
-cli.description('Artemis API Load Testing CLI');
-cli.name('artemis');
+cli.description("Artemis API Load Testing CLI");
+cli.name("artemis");
 
 cli
-  .command('run-test')
-  .requiredOption('-tc, --taskCount <number>', 'Task count.')
-  .requiredOption('-p, --path <path>', 'Relative path to test script file')
-  .description('Run the test script concurrently this number of times.')
+  .command("run-test")
+  .requiredOption("-tc, --taskCount <number>", "Task count.")
+  .requiredOption("-p, --path <path>", "Relative path to test script file")
+  .description("Run the test script concurrently this number of times.")
   .action(start);
+
 cli
-  .command('grafana-start')
-  .description('Start the Artemis Grafana dashboard.')
+  .command("grafana-start")
+  .description("Start the Artemis Grafana dashboard.")
   .action(startGrafana);
 
 cli
-  .command('grafana-stop')
-  .description('Stop the Artemis Grafana dashboard.')
+  .command("grafana-stop")
+  .description("Stop the Artemis Grafana dashboard.")
   .action(stopGrafana);
 
 cli
-  .command('deploy')
+  .command("deploy")
   .description("Deploy Artemis infrastructure on the user's AWS account.")
   .action(deploy);
 
 cli
-  .command('sleep')
+  .command("sleep")
   .description(
-    'Stop all support container tasks for minimal AWS usage charges.'
+    "Stop all support container tasks for minimal AWS usage charges."
   )
   .action(sleep);
 
 cli
-  .command('teardown')
+  .command("teardown")
+  .option("-y, --yes", "Execute teardown without user confirmation.")
   .description(
     "Teardown Artemis infrastructure on user's AWS account, retain Artemis database."
   )
   .action(teardown);
 
 cli
-  .command('destroy-db')
-  .description('Delete the Artemis database.')
+  .command("destroy-db")
+  .option("-y, --yes", "Execute destroy database without user confirmation.")
+  .description("Delete the Artemis database.")
   .action(destroyDb);
 
 cli.parse(process.argv);
--- src/utilities/teardownCommand.js
+++ src/utilities/teardownCommand.js
@@ -17,39 +17,45 @@ const pause = (time) => {
   return new Promise((resolve) => setTimeout(resolve, time)); // in ms
 };
 
-const startTeardown = async () => {
+const startTeardown = async (options) => {
   const USER_INPUT = ["y", "n", "yes", "no"];
   let confirmTeardown;
 
-  do {
-    confirmTeardown = readlineSync.question(
-      chalk.cyan("Are you sure you want to perform a teardown? (y/n)\n")
-    );
-    confirmTeardown = confirmTeardown.toLowerCase().trim();
+  if (options.yes) {
+    await exec(`cd ${cdkPathEscaped} && cdk destroy -f`, {
+      encoding: "utf-8",
+    });
+  } else {
+    do {
+      confirmTeardown = readlineSync.question(
+        chalk.cyan("Are you sure you want to perform a teardown? (y/n)\n")
+      );
+      confirmTeardown = confirmTeardown.toLowerCase().trim();
 
-    if (
-      USER_INPUT.includes(confirmTeardown) &&
-      confirmTeardown.startsWith("y")
-    ) {
-      await sleep();
-      const spinner = ora(chalk.cyan("Teardown in progress...")).start();
-      spinner.color = "yellow";
+      if (
+        USER_INPUT.includes(confirmTeardown) &&
+        confirmTeardown.startsWith("y")
+      ) {
+        await sleep();
+        const spinner = ora(chalk.cyan("Teardown in progress...")).start();
+        spinner.color = "yellow";
 
-      await exec(`cd ${cdkPathEscaped} && cdk destroy -f`, {
-        encoding: "utf-8",
-      });
-      spinner.succeed(chalk.cyan("Teardown completed successfully."));
-    } else if (
-      USER_INPUT.includes(confirmTeardown) &&
-      confirmTeardown.startsWith("n")
-    ) {
-      console.log(chalk.yellow("Teardown canceled."));
-    } else {
-      console.log(chalk.cyan("Please provide the correct input."));
-      await pause(1500);
-      console.clear();
-    }
-  } while (!USER_INPUT.includes(confirmTeardown));
+        await exec(`cd ${cdkPathEscaped} && cdk destroy -f`, {
+          encoding: "utf-8",
+        });
+        spinner.succeed(chalk.cyan("Teardown completed successfully."));
+      } else if (
+        USER_INPUT.includes(confirmTeardown) &&
+        confirmTeardown.startsWith("n")
+      ) {
+        console.log(chalk.yellow("Teardown canceled."));
+      } else {
+        console.log(chalk.cyan("Please provide the correct input."));
+        await pause(1500);
+        console.clear();
+      }
+    } while (!USER_INPUT.includes(confirmTeardown));
+  }
 };
 
 module.exports = { startTeardown };
