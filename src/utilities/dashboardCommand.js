d1111e7d79f41b7c9bf78f2b5f082df80d9480fa
Charles A. Hartman <charles@hartman.dev>
2022-04-19 17:19:17 -0700

    add delay to opening admin-dashboard, set config.json back to true, edit description of admin-dashboard command, add uploaded-scripts-folder and sample test script
    Co-authored-by: miajim <mjimenez@vt.edu>
    Co-authored-by: romero-c-raul <raulromerocano@aggienetwork.com>
    Co-authored-by: charleshartman <charles@hartman.dev>
    Co-authored-by: sophiemcgarity <sophiemcgarity7@gmail.com>

--- src/utilities/dashboardCommand.js
+++ src/utilities/dashboardCommand.js
@@ -1,14 +1,16 @@
-const path = require("path");
-const execSync = require("child_process").execSync;
-const { promisify } = require("util");
-const exec = promisify(require("child_process").exec);
-const dashboardPath = path.join(__dirname, "../../admin-dashboard/api/build");
-const open = require("open");
-const filepathEscaped = dashboardPath.replace(/ /g, "\\ ");
+const path = require('path');
+const execSync = require('child_process').execSync;
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+const dashboardPath = path.join(__dirname, '../../admin-dashboard/api/build');
+const open = require('open');
+const filepathEscaped = dashboardPath.replace(/ /g, '\\ ');
 
 const startDashboard = async () => {
   exec(`cd ${filepathEscaped} && npm start`);
-  await open("http://localhost:9000");
+  setTimeout(() => {
+    open('http://localhost:9000');
+  }, 5000);
 };
 
 module.exports = { startDashboard };
