ac0bae47414054a7908cbe7a3d6a48eeae996d2e
Charles A. Hartman <charles@hartman.dev>
2022-04-19 21:11:58 -0700

    added spinner and confirmation to dashboard command, moved package.json packages from /api to parent, only script remains in the api package.json, versioned up to 1.10 for eventual publishing

--- src/utilities/dashboardCommand.js
+++ src/utilities/dashboardCommand.js
@@ -2,14 +2,24 @@ const path = require('path');
 const execSync = require('child_process').execSync;
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
+const ora = require('ora-classic');
+const chalk = require('chalk');
 const dashboardPath = path.join(__dirname, '../../admin-dashboard/api/build');
 const open = require('open');
 const filepathEscaped = dashboardPath.replace(/ /g, '\\ ');
 
 const startDashboard = async () => {
   exec(`cd ${filepathEscaped} && npm start`);
+  const spinner = ora(chalk.cyan('Launching admin dashboard...')).start();
+  spinner.color = 'yellow';
   setTimeout(() => {
     open('http://localhost:9000');
+    spinner.succeed(
+      chalk.cyan(
+        'Admin dashboard running in the browser at http://localhost:9000.'
+      )
+    );
+    console.log(chalk.green('Ctrl-C to stop the admin dashboard.'));
   }, 5000);
 };
 
